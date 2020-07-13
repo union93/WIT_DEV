7f6db2b5020cd76d2548beffca06683ba37067f1
union93 <union93@konkuk.ac.kr>
2020-07-13 19:21:27 +0900

    first update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from accounts.forms import SignUpForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        signup_form = SignUpForm(request.POST)
+
+        if signup_form.is_valid():
+            user_instance = signup_form.save(commit=False)
+            user_instance.set_password(signup_form.cleaned_data['password'])
+            user_instance.save()
+            return render(request, 'accounts.signup_complete.html', {'username': user_instance.username})
+
+    else:
+        signup_form = SignUpForm()
+
+    return render(request, 'accounts.signup_complete.html', {'form': signup_form.as_p})
